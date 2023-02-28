7a2f37a432ee833b3bfeacf02bc7a4fdd20fe450
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-02-28 14:15:20 -0600

    const type

--- driver/src/client/client_data.hpp
+++ driver/src/client/client_data.hpp
@@ -6,6 +6,6 @@ class ClientData {
         int port;
         std::string device_name;
         std::string bssid;
-        char* serializeData();
+        const char* serializeData();
         ClientData(std::string ip, int port, std::string device_name, std::string bssid);
 };
