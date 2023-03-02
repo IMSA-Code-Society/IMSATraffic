270a467d2abce76434db8a02b2c622cc2206e4b4
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-02 14:37:11 -0600

    sending data test

--- driver/src/main.cpp
+++ driver/src/main.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+#include <string>
+#include "client/driver_client.hpp"
+#include "client/client_data.hpp"
 
+std::string IP = "127.0.0.1";
+int PORT = 3003;
 
 int main() {
+    std::cout << "Starting client" << std::endl;
+
+    DriverClient client;
+    client.connectClient(IP, PORT);
+
+    ClientData data("test", "12:45:78:01:34:67");
+    client.sendData(data);
+
     return 0;
 }
