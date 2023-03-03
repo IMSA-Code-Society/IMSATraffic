173c27a2f34ab304adf41ed9ff891b3b994da66a
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-03 15:23:46 -0600

    switched bssid and name in packet

--- driver/src/client/client_data.cpp
+++ driver/src/client/client_data.cpp
@@ -11,12 +11,12 @@ const char* ClientData::serializeData() {
     const char* device_data = device_name.c_str();
     const char* bssid_data = bssid.c_str();
 
-    int data_size = device_name.length() + bssid.length();
+    int data_size = bssid.length() + device_name.length();
     char* serialized_data = new char[data_size];
     serialized_data[0] = 0; 
 
-    strcat(serialized_data, device_data);
     strcat(serialized_data, bssid_data);
+    strcat(serialized_data, device_data);
 
     return (const char*)serialized_data;
 }
