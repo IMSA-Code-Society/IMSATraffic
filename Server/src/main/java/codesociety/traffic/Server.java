4db0a5e16f1658dad23f09c63014af2cbc38a0be
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-12 18:12:13 -0500

    driver server

--- Server/src/main/java/codesociety/traffic/Server.java
+++ Server/src/main/java/codesociety/traffic/Server.java
@@ -1,14 +1,24 @@
 package codesociety.traffic;
 
-import codesociety.traffic.network.WebServer;
+import codesociety.traffic.driver.DriverServer;
 import codesociety.traffic.util.ConfigReader;
+import codesociety.traffic.util.DatabaseIO;
+import codesociety.traffic.web.WebServer;
 
 public class Server 
 {
     public static void main( String[] args )
     {
+        System.out.println("Starting server...");
+
+        DatabaseIO db = new DatabaseIO();
+
         ConfigReader config = new ConfigReader();
-        WebServer server = new WebServer(config);
-        server.start();
+        WebServer webServer = new WebServer(config);
+        webServer.start();
+
+        System.out.println(config.driverPort);
+        DriverServer driverServer = new DriverServer(config.driverPort, db);
+        driverServer.start();
     }
 }
