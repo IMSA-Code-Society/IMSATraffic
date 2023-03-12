3556e948cd617c9b6bc19d3cb59633d94fdf2f25
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-12 18:10:17 -0500

    logging bssid every 3 minutes

--- driver/src/main.cpp
+++ driver/src/main.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 #include "driver_client.hpp"
 #include "client_data.hpp"
+#include "bssid_logger.hpp"
 
 std::string IP = "127.0.0.1";
 int PORT = 3003;
+int INTERVAL = 3 * 60 * 1000;
+
+std::string last_bssid;
+std::string device_name;
 
 int main() {
-    std::cout << "Starting client" << std::endl;
+    std::cout << "starting client" << std::endl;
+    std::cout << IP << ":" << PORT << std::endl;
 
     DriverClient client;
     client.connectClient(IP, PORT);
 
-    ClientData data("test", "12:45:78:01:34:67");
-    client.sendData(data);
+    while(true) {
+        BSSIDLogger logger;
+        std::string bssid = logger.getBSSID();
+
+        Sleep(INTERVAL);
+
+        if (bssid != last_bssid) {
+            device_name = ClientData::generateDeviceName();
+            last_bssid = bssid;
+        }
+
+        ClientData data(device_name, bssid); // "12:45:78:01:34:67"
+        client.sendData(data);
+    }
 
     return 0;
 }
