4a0ba2a875b2fac0faedcd33a7e987bfcfa24563
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-14 21:51:23 -0500

    uninstall script

---
+++ driver/scripts/uninstall.bat
@@ -0,0 +1,5 @@
+@taskkill /f /im TrafficDriver.exe >nul 2>&1
+@del "%USERPROFILE%\Start Menu\Programs\Startup\traffic_updater.bat" >nul 2>&1
+@del "%USERPROFILE%\Start Menu\Programs\Startup\TrafficDriver.exe" >nul 2>&1
+@echo Uninstall finished
+@pause
