0a8ed9afa331b91f83c9a00f1e17feab66729fa6
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-14 21:42:22 -0500

    copy and zip all files

---
+++ driver/cmake/ziplist.cmake
@@ -0,0 +1,6 @@
+set(ZIP_FILES ${ZIP_FILES} 
+    "${CMAKE_BINARY_DIR}/TrafficDriver/install.bat"
+    "${CMAKE_BINARY_DIR}/TrafficDriver/uninstall.bat"
+    "${CMAKE_BINARY_DIR}/TrafficDriver/bin/traffic_updater.bat"
+    "${CMAKE_BINARY_DIR}/TrafficDriver/bin/TrafficDriver.exe"
+)
