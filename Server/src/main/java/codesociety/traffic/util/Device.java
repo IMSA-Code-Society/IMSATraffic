c137d400cb63ba95326192a1699c80be81a163a5
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-29 14:53:34 -0500

    device class

---
+++ Server/src/main/java/codesociety/traffic/util/Device.java
@@ -0,0 +1,11 @@
+package codesociety.traffic.util;
+
+public class Device {
+    public String id;
+    public long timestamp;
+    
+    public Device(String id, long timestamp) {
+        this.id = id;
+        this.timestamp = timestamp;
+    }
+}
