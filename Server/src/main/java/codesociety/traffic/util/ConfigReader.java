3ebd5e55d646f54e6c1297810ce1d8874c1e3c13
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-29 14:52:10 -0500

    db config

--- Server/src/main/java/codesociety/traffic/util/ConfigReader.java
+++ Server/src/main/java/codesociety/traffic/util/ConfigReader.java
@@ -10,17 +10,25 @@ public class ConfigReader {
 
     public int driverPort;
 
+    public String dbURL;
+    public String dbUser;
+    public String dbPassword;
+
     public ConfigReader() {
         FileInputStream serverStream;
         Properties serverProp = new Properties();
         FileInputStream driverStream;
         Properties driverProp = new Properties();
+        FileInputStream dbStream;
+        Properties dbProp = new Properties();
         
         try {
             serverStream = new FileInputStream("server/config/server.config");
             serverProp.load(serverStream);
             driverStream = new FileInputStream("server/config/driver.config");
             driverProp.load(driverStream);
+            dbStream = new FileInputStream("server/config/db.config");
+            dbProp.load(dbStream);
         } catch (FileNotFoundException err) {
             err.printStackTrace();
         } catch (IOException err) {
@@ -28,6 +36,11 @@ public class ConfigReader {
         }
 
         this.webPort = Integer.parseInt(serverProp.getProperty("port"));
+
         this.driverPort = Integer.parseInt(driverProp.getProperty("port"));
+        
+        this.dbURL = driverProp.getProperty("url");
+        this.dbUser = driverProp.getProperty("user");
+        this.dbPassword = driverProp.getProperty("password");
     }
 }
