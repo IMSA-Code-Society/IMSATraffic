0e9f356fe1a16706ea106f0a8eed64bd63dae372
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-03-29 14:52:56 -0500

    postgresql db

--- Server/src/main/java/codesociety/traffic/util/DatabaseIO.java
+++ Server/src/main/java/codesociety/traffic/util/DatabaseIO.java
@@ -1,16 +1,68 @@
 package codesociety.traffic.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
 import codesociety.traffic.driver.DriverData;
 
 public class DatabaseIO {
-    public DatabaseIO() {
+    private Hashtable<String, List<Device>> rooms = new Hashtable<>();
+    private Connection conn;
 
+    public DatabaseIO(ConfigReader config) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Properties props = new Properties();
+        props.setProperty("user", config.dbUser);
+        props.setProperty("password", config.dbPassword);
+        props.setProperty("ssl", "false");
+
+        try {
+            conn = DriverManager.getConnection(config.dbURL, props);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
     public void writeDriverData(DriverData data) {
-        System.out.println(data.bssid);
-        System.out.println(data.deviceName);
+        List<Device> deviceList = rooms.get(data.bssid);
 
-        
+        if (deviceList == null) {
+            rooms.put(data.bssid, new ArrayList<>());
+        }
+
+        for (int i = 0; i < deviceList.size(); i++) {
+            if (deviceList.get(i).id == data.deviceName) {
+                return;
+            }
+        }
+
+        deviceList.add(new Device(data.deviceName, System.currentTimeMillis()));
+        rooms.put(data.bssid, deviceList);
+    }
+
+    public void executeQuery(String statement) {
+        try {
+            Statement st = conn.createStatement();
+            st.executeQuery(statement);
+            st.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public Hashtable<String, List<Device>> getRooms() {
+        return rooms;
     }
 }
