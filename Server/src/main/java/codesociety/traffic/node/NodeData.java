ef36074ed31b177141e8870baf4c016402797f8c
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-04-17 20:04:19 -0500

    node data

---
+++ Server/src/main/java/codesociety/traffic/node/NodeData.java
@@ -0,0 +1,11 @@
+package codesociety.traffic.node;
+
+public class NodeData {
+    public String room;
+    public float value;
+
+    public NodeData(String room, float value) {
+        this.room = room;
+        this.value = value;
+    }
+}
