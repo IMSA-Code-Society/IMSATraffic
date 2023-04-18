925de2e82accf280515b35f23e7308e579a377ad
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-04-17 23:01:48 -0500

    history request placeholder

--- Server/src/main/java/codesociety/traffic/web/WebServer.java
+++ Server/src/main/java/codesociety/traffic/web/WebServer.java
@@ -6,10 +6,9 @@ import codesociety.traffic.node.NodeData;
 import codesociety.traffic.util.ConfigReader;
 import codesociety.traffic.util.DatabaseIO;
 import codesociety.traffic.util.DeviceCounter;
+import static codesociety.traffic.util.StringUtils.*;
 
 public class WebServer {
-    private String[] roomLabels = new String[] {"in2", "oldCaf", "irc", "tvPit", "ssa", "loft", "msa", "studion"};
-
     private ConfigReader config;
     private DatabaseIO db;
     private DeviceCounter counter;
@@ -26,12 +25,7 @@ public class WebServer {
         init();
 
         get("/traffic.json", (req, res) -> {
-            String json = "{\n";
-            for (int i = 0; i < roomLabels.length; i++) {
-                json += "\t\"" + roomLabels[i] + "\": " + counter.counts[i] + ",\n";
-            }
-            json = json.replaceFirst(".$","");
-            json += "}";
+            String json = generateRoomJson(floatArrayToStringArray(counter.counts));
             res.type("application/json");
             return json;
         });
@@ -41,6 +35,16 @@ public class WebServer {
             float value = Float.parseFloat(req.queryParams("value"));
             db.writeNodeData(new NodeData(area, value));
             return "success";
+        });
+
+        get("/history.json", (req, res) -> {
+            float[][] history = db.getHistory();
+            String[] historyArrays = new String[8];
+            for (int i = 0; i < 8; i++) {
+                historyArrays[i] = stringifyFloatArray(history[i]);
+            }
+            String json = generateRoomJson(historyArrays);
+            return json;
         });
     }
 }
