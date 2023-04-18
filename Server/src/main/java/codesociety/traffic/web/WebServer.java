65c48de52302030847972908628d45340bcd4c50
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-04-17 20:03:45 -0500

    node data

--- Server/src/main/java/codesociety/traffic/web/WebServer.java
+++ Server/src/main/java/codesociety/traffic/web/WebServer.java
@@ -2,17 +2,21 @@ package codesociety.traffic.web;
 
 import static spark.Spark.*;
 
+import codesociety.traffic.node.NodeData;
 import codesociety.traffic.util.ConfigReader;
+import codesociety.traffic.util.DatabaseIO;
 import codesociety.traffic.util.DeviceCounter;
 
 public class WebServer {
     private String[] roomLabels = new String[] {"in2", "oldCaf", "irc", "tvPit", "ssa", "loft", "msa", "studion"};
 
     private ConfigReader config;
+    private DatabaseIO db;
     private DeviceCounter counter;
 
-    public WebServer(ConfigReader config, DeviceCounter counter) {
+    public WebServer(ConfigReader config, DatabaseIO db, DeviceCounter counter) {
         this.config = config;
+        this.db = db;
         this.counter = counter;
     }
 
@@ -30,6 +34,13 @@ public class WebServer {
             json += "}";
             res.type("application/json");
             return json;
+        });
+
+        get("/area/*", (req, res) -> {
+            String area = req.splat()[0];
+            float value = Float.parseFloat(req.queryParams("value"));
+            db.writeNodeData(new NodeData(area, value));
+            return "success";
         });
     }
 }
